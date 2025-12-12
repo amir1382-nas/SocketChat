2dc28c47cf18d8c5957a3b019fb6cdafa0337196
amir1382-nas <amir.nas1382@gmail.com>
2025-12-13 02:24:33 +0330

    new file

---
+++ client.py
@@ -0,0 +1,11 @@
+import socket
+
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client_socket.connect("127.0.0.1", 5000)
+
+client_socket.send("Hi".encode("utf-8"))
+
+reply = client_socket.recv(1024).decode("utf-8")
+print(f"CLIENT Server replied: {reply}")
+
+client_socket.close()
