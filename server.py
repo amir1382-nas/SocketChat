9e7cf13879438258ad2ce21795e303cc4151352e
amir1382-nas <amir.nas1382@gmail.com>
2025-12-13 02:27:40 +0330

    update server code

--- server.py
+++ server.py
@@ -1,7 +1,7 @@
 import socket
 
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server_socket.bind("127.0.0.1", 5000)
+server_socket.bind(("127.0.0.1", 5000))
 server_socket.listen(1)
 print("[SERVER] Listening on port 5000 ...")
 
